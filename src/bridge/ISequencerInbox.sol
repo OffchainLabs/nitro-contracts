c5391f6c2036b759592b12cf8b181fd7ba98c15a
shotaronowhere <shawtarohgn@gmail.com>
2023-06-12 14:00:33 +0900

    feat(SequencerInbox): gas optimization

--- src/bridge/ISequencerInbox.sol
+++ src/bridge/ISequencerInbox.sol
@@ -12,10 +12,10 @@ import "./IBridge.sol";
 
 interface ISequencerInbox is IDelayedMessageProvider {
     struct MaxTimeVariation {
-        uint256 delayBlocks;
-        uint256 futureBlocks;
-        uint256 delaySeconds;
-        uint256 futureSeconds;
+        uint64 delayBlocks;
+        uint64 futureBlocks;
+        uint64 delaySeconds;
+        uint64 futureSeconds;
     }
 
     struct TimeBounds {
@@ -80,10 +80,10 @@ interface ISequencerInbox is IDelayedMessageProvider {
         external
         view
         returns (
-            uint256,
-            uint256,
-            uint256,
-            uint256
+            uint64,
+            uint64,
+            uint64,
+            uint64
         );
 
     function dasKeySetInfo(bytes32) external view returns (bool, uint64);
