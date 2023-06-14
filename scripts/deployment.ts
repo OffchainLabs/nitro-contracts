8c92856808a8bad4e4048f5f35f94f0cdc7a38bd
GreatSoshiant <salehi.mehdi70@gmail.com>
2023-06-14 11:18:37 -0700

    adding outbox address

--- scripts/deployment.ts
+++ scripts/deployment.ts
@@ -1,5 +1,5 @@
 import { ethers } from 'hardhat'
-import { ContractFactory, Contract, providers, Wallet } from 'ethers'
+import { ContractFactory, Contract} from 'ethers'
 import '@nomiclabs/hardhat-ethers'
 
 interface RollupCreatedEvent {
@@ -155,6 +155,10 @@ async function main() {
       console.log(
         'ValidatorWalletCreator Contract created at address:',
         contracts.validatorWalletCreator.address
+      )
+      console.log(
+        'outbox Contract created at address:',
+        await contracts.rollup.outbox()
       )
 
       // getting the block number
