1e31d42396af2b231b915873e2085880560b396f
Joshua Colvin <jcolvin@offchainlabs.com>
2023-08-28 14:31:33 -0700

    Add codehashversion precompile

--- src/precompiles/ArbWasm.sol
+++ src/precompiles/ArbWasm.sol
@@ -18,6 +18,10 @@ interface ArbWasm {
     // @return version the stylus version
     function stylusVersion() external view returns (uint16 version);
 
+    // @notice gets the stylus version the program with codehash was most recently compiled against.
+    // @return version the program version (0 for EVM contracts)
+    function codehashVersion(bytes32 codehash) external view returns (uint16 version);
+
     // @notice gets the stylus version the program was most recently compiled against.
     // @return version the program version (0 for EVM contracts)
     function programVersion(address program) external view returns (uint16 version);
