718c185e4c1c66c06eaf604483da225fd6b8c5de
Goran Vladika <goran.vladika@gmail.com>
2023-09-12 10:34:06 +0200

    Typo

--- src/rollup/RollupCreator.sol
+++ src/rollup/RollupCreator.sol
@@ -94,7 +94,7 @@ contract RollupCreator is Ownable {
      * @param _validators  The list of validator addresses, not used when set to empty list
      * @param _nativeToken Address of the custom fee token used by rollup. If rollup is ETH-based address(0) should be provided
      * @param _deployFactoriesToL2 Whether to deploy L2 factories using retryable tickets. If true, retryables need to be paid for in native currency.
-     *                             Deploying factories via retyrable tickets at rollup creation time is the most reliable method to do it since it
+     *                             Deploying factories via retryable tickets at rollup creation time is the most reliable method to do it since it
      *                             doesn't require paying the L1 gas. If deployment is not done as part of rollup creation TX, there is a risk that
      *                             anyone can try to deploy factories and potentially burn the nonce 0 (ie. due to gas price spike when doing direct
      *                             L2 TX). That would mean we permanently lost capability to deploy deterministic factory at expected address.
