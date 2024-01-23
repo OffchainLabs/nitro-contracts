66aba4e03e3b49b921a404ab9b47cfbcd171504b
gzeon <im@gzeon.dev>
2024-01-23 17:50:02 +0900

    test: update DATA_BLOB_HEADER_FLAG

--- test/contract/toolkit4844.ts
+++ test/contract/toolkit4844.ts
@@ -11,7 +11,7 @@ const wait = async (ms: number) =>
   })
 
 export class Toolkit4844 {
-  public static DATA_BLOB_HEADER_FLAG = '0x40'
+  public static DATA_BLOB_HEADER_FLAG = '0x50' // 0x40 | 0x10
 
   public static postDataToGeth(body: any): Promise<any> {
     return new Promise((resolve, reject) => {
