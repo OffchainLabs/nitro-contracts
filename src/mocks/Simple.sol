f3a1326f663458f471b2ddcd60460d34b6ab71f8
gzeon <im@gzeon.dev>
2024-02-10 01:25:42 +0800

    chore: swap ignore

--- src/mocks/Simple.sol
+++ src/mocks/Simple.sol
@@ -130,8 +130,8 @@ contract Simple {
         uint256 before = gasleft();
         // The inner call may revert, but we still want to return the amount of gas used,
         // so we ignore the result of this call.
-        // solc-ignore-next-line unused-call-retval
         // solhint-disable-next-line avoid-low-level-calls
+        // solc-ignore-next-line unused-call-retval
         to.staticcall{gas: before - 10000}(input);
         return before - gasleft();
     }
