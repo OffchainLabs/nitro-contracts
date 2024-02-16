3ce0a3d518dd3cda2da52105e0a514cf6c600495
Goran Vladika <goran.vladika@gmail.com>
2024-02-16 14:41:22 +0100

    Improve logging

--- test/gas-reporter/scripts/generateReferentGasReport.ts
+++ test/gas-reporter/scripts/generateReferentGasReport.ts
@@ -5,18 +5,23 @@ import { writeFileSync } from 'fs'
 dotenv.config()
 
 function main() {
+  /// get infura key to create RPC endpoint
   const infuraKey = process.env['INFURA_KEY'] as string
   if (!infuraKey) {
     throw new Error('INFURA_KEY env var should be set')
   }
   const mainnetRpc = `https://mainnet.infura.io/v3/${infuraKey}`
 
-  const referentGasReport = getGasSpendingRecord(mainnetRpc, true)
+  /// get the gas record for the current implementation
+  const referentGasReport = getGasSpendingRecord(mainnetRpc, false)
+
+  /// save the referent gas report
   try {
     writeFileSync(
       REFERENT_REPORT_FILE_PATH,
       JSON.stringify(referentGasReport, null, 2)
     )
+    console.log(`Referent gas report saved to ${REFERENT_REPORT_FILE_PATH}`)
   } catch (error) {
     console.error('An error occurred saving the referent gas report:', error)
   }
