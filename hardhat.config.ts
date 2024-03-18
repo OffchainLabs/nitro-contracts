b0c1f3170f7032cc23c836eebfb66c9b1a3288f3
Goran Vladika <goran.vladika@gmail.com>
2024-03-18 14:24:27 +0100

    Add hardhat config for testnode L1

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -138,6 +138,12 @@ module.exports = {
     geth: {
       url: 'http://localhost:8545',
     },
+    testnode_l1: {
+      url: 'http://localhost:8545',
+      accounts: process.env['DEPLOYER_PRIVKEY']
+        ? [process.env['DEPLOYER_PRIVKEY']]
+        : [],
+    },
   },
   etherscan: {
     apiKey: {
