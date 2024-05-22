aa56b8ba1c1b4c4ff9bdf90bf6bb6ecb3f952222
ImJeremyHe <yiliang.he@qq.com>
2024-05-22 15:18:12 +0800

    Add IS_HOTSHOT_ALIVE opcode

--- src/test-helpers/HotShot.sol
+++ src/test-helpers/HotShot.sol
@@ -3,8 +3,13 @@ pragma solidity ^0.8.0;
 
 contract MockHotShot {
     mapping(uint256 => uint256) public commitments;
+    mapping(uint256 => bool) public availabilities;
 
     function setCommitment(uint256 height, uint256 commitment) external {
         commitments[height] = commitment;
+    }
+
+    function setAvailability(uint256 l1Height, bool isAlive) external {
+        availabilities[l1Height] = isAlive;
     }
 }
