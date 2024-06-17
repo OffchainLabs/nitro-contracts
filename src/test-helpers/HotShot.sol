9837543b05aeab8e022523d1fa927d390e9540b4
ImJeremyHe <yiliang.he@qq.com>
2024-06-17 10:22:49 +0800

    Update hotshot contract interface

--- src/test-helpers/HotShot.sol
+++ src/test-helpers/HotShot.sol
@@ -1,15 +1,38 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
-contract MockHotShot {
+import "../bridge/IHotShot.sol";
+
+contract MockHotShot is IHotShot {
     mapping(uint256 => uint256) public commitments;
-    mapping(uint256 => bool) public availabilities;
+    mapping(uint256 => bool) public livenesses;
+
+    function getHotShotCommitment(uint256 hotShotBlockHeight)
+        external
+        view
+        override
+        returns (HotShotCommitment memory)
+    {
+        return HotShotCommitment({
+            blockHeight: uint64(hotShotBlockHeight),
+            blockCommRoot: commitments[hotShotBlockHeight]
+        });
+    }
+
+    function lagOverEscapeHatchThreshold(uint256 blockNumber, uint256 threshold)
+        external
+        view
+        override
+        returns (bool)
+    {
+        return true;
+    }
 
     function setCommitment(uint256 height, uint256 commitment) external {
         commitments[height] = commitment;
     }
 
-    function setAvailability(uint256 l1Height, bool isLive) external {
-        availabilities[l1Height] = isLive;
+    function setLiveness(uint256 l1Height, bool isLive) external {
+        livenesses[l1Height] = isLive;
     }
 }
