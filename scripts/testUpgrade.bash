94c31ef1fb28b9f18a0bf776e96ccd1fbff805b3
Henry <11198460+godzillaba@users.noreply.github.com>
2024-07-01 15:58:51 -0400

    ...

--- scripts/testUpgrade.bash
+++ scripts/testUpgrade.bash
@@ -4,10 +4,6 @@ anvil --fork-url $L1_RPC > /dev/null &
 
 anvil_pid=$!
 
-sleep 5
-
-cast chain-id
-
 yarn script:bold-prepare && \
 yarn script:bold-populate-lookup && \
 yarn script:bold-local-execute
