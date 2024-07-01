ce13dcfadb811d3f473b372d143af5ed510b49ef
Henry <11198460+godzillaba@users.noreply.github.com>
2024-07-01 15:42:33 -0400

    proper exit

--- scripts/testUpgrade.bash
+++ scripts/testUpgrade.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-exit 1
-
-set -e
 anvil --fork-url $L1_RPC > /dev/null &
 
 anvil_pid=$!
 
-yarn script:bold-prepare
-yarn script:bold-populate-lookup
+yarn script:bold-prepare && \
+yarn script:bold-populate-lookup && \
 yarn script:bold-local-execute
 
+ecode=$? 
+
 kill $anvil_pid
+
+exit $ecode
