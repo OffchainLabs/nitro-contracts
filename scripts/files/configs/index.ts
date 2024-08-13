309a745bed228127740aab2f6d0dd0374d90beb3
gzeon <im@gzeon.dev>
2024-08-14 00:43:36 +0900

    feat: safer hoursToBlocks util

--- scripts/files/configs/index.ts
+++ scripts/files/configs/index.ts
@@ -8,6 +8,9 @@ export function hoursToBlocks(hours: number, blockTime = 12) {
   if (x !== Math.floor(x)) {
     throw new Error('hours must be divisible by blockTime')
   }
+  if (x === 0) {
+    throw new Error('hours must be greater than 0')
+  }
   return x
 }
 
