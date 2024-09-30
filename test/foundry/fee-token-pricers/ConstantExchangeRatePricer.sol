960d7da9958ef8e869f1544615fc184fef70f733
Goran Vladika <goran.vladika@gmail.com>
2024-09-30 09:29:52 +0200

    Add ConstantExchangeRatePricer

---
+++ test/foundry/fee-token-pricers/ConstantExchangeRatePricer.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IFeeTokenPricer} from "../../../src/bridge/ISequencerInbox.sol";
+
+/**
+ * @title Test implementation of a fee token pricer that returns a constant exchange rate
+ * @notice Exchange rate is set in constructor and cannot be changed
+ */
+contract ConstantExchangeRatePricer is IFeeTokenPricer {
+    uint256 immutable exchangeRate;
+
+    constructor(uint256 _exchangeRate) {
+        exchangeRate = _exchangeRate;
+    }
+
+    // @inheritdoc IFeeTokenPricer
+    function getExchangeRate() external view returns (uint256) {
+        return exchangeRate;
+    }
+}
