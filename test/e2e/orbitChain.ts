10ba0af9a2e9d804db5b1bbd1a4f3373c0f76f7a
gzeon <hng@offchainlabs.com>
2024-10-01 01:28:27 +0800

    fix: orbit e2e (#257)
    Co-authored-by: Chris Buckland <cpbuckland88@gmail.com>

--- test/e2e/orbitChain.ts
+++ test/e2e/orbitChain.ts
@@ -25,6 +25,10 @@ import {
 import { getLocalNetworks } from '../../scripts/testSetup'
 import { applyAlias } from '../contract/utils'
 import { BigNumber, ContractTransaction, Wallet, ethers } from 'ethers'
+import {
+  l1Networks,
+  l2Networks,
+} from '@arbitrum/sdk/dist/lib/dataEntities/networks'
 
 const LOCALHOST_L2_RPC = 'http://127.0.0.1:8547'
 const LOCALHOST_L3_RPC = 'http://127.0.0.1:3347'
@@ -69,10 +73,18 @@ describe('Orbit Chain', () => {
         l2WethGateway: '',
       },
     }
-    addCustomNetwork({
-      customL1Network: l1Network,
-      customL2Network: l2Network,
-    })
+    if (!l2Networks[l2Network.chainID.toString()]) {
+      if (!l1Networks[l2Network.chainID.toString()]) {
+        addCustomNetwork({
+          customL1Network: l1Network,
+          customL2Network: l2Network,
+        })
+      } else {
+        addCustomNetwork({
+          customL2Network: l2Network,
+        })
+      }
+    }
 
     l1Provider = new JsonRpcProvider(LOCALHOST_L2_RPC)
     l2Provider = new JsonRpcProvider(LOCALHOST_L3_RPC)
@@ -643,7 +655,10 @@ describe('Orbit Chain', () => {
 
     const inbox = l2Network.ethBridge.inbox
     const maxFeePerGas = BigNumber.from('100000000') // 0.1 gwei
-    let fee = await deployHelper.getDeploymentTotalCost(inbox, maxFeePerGas)
+    let fee = await deployHelper.getDeploymentTotalCost(inbox, maxFeePerGas, {
+      from: userL1Wallet.address,
+      gasPrice: maxFeePerGas,
+    })
 
     if (nativeToken) {
       const decimals = await nativeToken.decimals()
@@ -688,7 +703,6 @@ describe('Orbit Chain', () => {
       ).wait()
     }
 
-    // deploy factories
     const receipt = await (
       await deployHelper
         .connect(userL1Wallet)
@@ -737,7 +751,10 @@ describe('Orbit Chain', () => {
 
     const inbox = l2Network.ethBridge.inbox
     const maxFeePerGas = BigNumber.from('100000000') // 0.1 gwei
-    let fee = await deployHelper.getDeploymentTotalCost(inbox, maxFeePerGas)
+    let fee = await deployHelper.getDeploymentTotalCost(inbox, maxFeePerGas, {
+      from: userL1Wallet.address,
+      gasPrice: maxFeePerGas,
+    })
     if (nativeToken) {
       const decimals = await nativeToken.decimals()
       if (decimals < 18) {
