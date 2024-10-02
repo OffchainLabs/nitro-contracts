0cb90fdfc66a6b8a740f7cc0ca110a6ddb4fef0e
Goran Vladika <goran.vladika@gmail.com>
2024-10-02 08:49:34 +0200

    Return exchange rate

--- test/foundry/fee-token-pricers/uniswap-v2-twap/UniswapV2TwapPricer.sol
+++ test/foundry/fee-token-pricers/uniswap-v2-twap/UniswapV2TwapPricer.sol
@@ -2,19 +2,20 @@
 pragma solidity ^0.8.0;
 
 import {IFeeTokenPricer} from "../../../../src/bridge/ISequencerInbox.sol";
-import "@uniswap/v2-core/interfaces/IUniswapV2Factory.sol";
-import "@uniswap/v2-core/interfaces/IUniswapV2Pair.sol";
 import {FixedPoint} from "./FixedPoint.sol";
+import {IUniswapV2Pair} from "@uniswap/v2-core/interfaces/IUniswapV2Pair.sol";
 
 /**
  * @title Test implementation of a fee token pricer that uses Uniswap V2 TWAP
  */
 contract UniswapV2TwapPricer is IFeeTokenPricer {
+    using FixedPoint for *;
+
     uint256 public constant TWAP_WINDOW = 1 hours;
 
     IUniswapV2Pair immutable pair;
-    address public immutable token0;
-    address public immutable token1;
+    address public immutable weth;
+    address public immutable token;
 
     uint256 public price0CumulativeLast;
     uint256 public price1CumulativeLast;
@@ -23,10 +24,16 @@ contract UniswapV2TwapPricer is IFeeTokenPricer {
     FixedPoint.uq112x112 public price0Average;
     FixedPoint.uq112x112 public price1Average;
 
-    constructor(IUniswapV2Pair _pair) {
+    constructor(IUniswapV2Pair _pair, address _weth) {
         pair = _pair;
-        token0 = _pair.token0();
-        token1 = _pair.token1();
+        address token0 = _pair.token0();
+        address token1 = _pair.token1();
+
+        require(token0 == _weth || token1 == _weth, "WETH not in pair");
+
+        weth = _weth;
+        token = token0 == _weth ? token1 : token0;
+
         price0CumulativeLast = _pair.price0CumulativeLast();
         price1CumulativeLast = _pair.price1CumulativeLast();
         uint112 reserve0;
@@ -41,25 +48,26 @@ contract UniswapV2TwapPricer is IFeeTokenPricer {
         uint32 timeElapsed = currentBlockTimestamp - pricerUpdatedAt;
 
         if (timeElapsed >= TWAP_WINDOW) {
-            _update();
+            _update(timeElapsed);
         }
 
-        // calculate exchange rate
-
-        return 0;
+        if (weth == pair.token0()) {
+            return FixedPoint.mul(price0Average, uint256(1)).decode144();
+        } else {
+            return FixedPoint.mul(price1Average, uint256(1)).decode144();
+        }
     }
 
-    function update() external {
+    function updatePrice() external {
         uint32 currentBlockTimestamp = uint32(block.timestamp);
         uint32 timeElapsed = currentBlockTimestamp - pricerUpdatedAt;
         require(timeElapsed >= TWAP_WINDOW, "Minimum TWAP window not elapsed");
 
-        _update();
+        _update(timeElapsed);
     }
 
-    function _update() internal {
+    function _update(uint256 timeElapsed) internal {
         uint32 currentBlockTimestamp = uint32(block.timestamp);
-        uint32 timeElapsed = currentBlockTimestamp - pricerUpdatedAt;
 
         // fetch latest cumulative price accumulators
         IUniswapV2Pair _pair = pair;
@@ -79,10 +87,14 @@ contract UniswapV2TwapPricer is IFeeTokenPricer {
 
         // overflow is desired, casting never truncates
         // cumulative price is in (uq112x112 price * seconds) units so we simply wrap it after division by time elapsed
-        price0Average =
-            FixedPoint.uq112x112(uint224((price0Cumulative - price0CumulativeLast) / timeElapsed));
-        price1Average =
-            FixedPoint.uq112x112(uint224((price1Cumulative - price1CumulativeLast) / timeElapsed));
+        unchecked {
+            price0Average = FixedPoint.uq112x112(
+                uint224((price0Cumulative - price0CumulativeLast) / timeElapsed)
+            );
+            price1Average = FixedPoint.uq112x112(
+                uint224((price1Cumulative - price1CumulativeLast) / timeElapsed)
+            );
+        }
 
         price0CumulativeLast = price0Cumulative;
         price1CumulativeLast = price1Cumulative;
