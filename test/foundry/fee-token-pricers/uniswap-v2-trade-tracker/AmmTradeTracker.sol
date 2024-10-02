640a497ca6e8899d2f6293ac5baf01536bd5d382
Goran Vladika <goran.vladika@gmail.com>
2024-10-02 09:48:19 +0200

    AMM trade tracker draft

---
+++ test/foundry/fee-token-pricers/uniswap-v2-trade-tracker/AmmTradeTracker.sol
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {IFeeTokenPricer} from "../../../../src/bridge/ISequencerInbox.sol";
+
+/**
+ * @title Test implementation of a fee token pricer that trades on AMM and keeps track of trades
+ */
+contract AmmTradeTracker is IFeeTokenPricer {
+    IUniswapV2Router01 public immutable router;
+    address public immutable token;
+    address public immutable weth;
+
+    uint256 public totalEthReceived;
+    uint256 public totalTokenSpent;
+
+    constructor(IUniswapV2Router01 _router, address _token) {
+        router = _router;
+        token = _token;
+        weth = _router.WETH();
+    }
+
+    // @inheritdoc IFeeTokenPricer
+    function getExchangeRate() external returns (uint256) {}
+
+    function _swap(uint256 tokenAmount) internal {
+        IERC20(token).approve(address(router), tokenAmount);
+
+        address[] memory path = new address[](2);
+        path[0] = token;
+        path[1] = weth;
+
+        uint256[] memory amounts = router.swapExactTokensForETH({
+            amountIn: tokenAmount,
+            amountOutMin: 1,
+            path: path,
+            to: msg.sender,
+            deadline: block.timestamp
+        });
+        uint256 ethReceived = amounts[amounts.length - 1];
+
+        totalEthReceived += ethReceived;
+        totalTokenSpent += tokenAmount;
+    }
+}
+
+interface IUniswapV2Router01 {
+    function WETH() external pure returns (address);
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+}
+
+interface IERC20 {
+    function approve(address spender, uint256 value) external returns (bool);
+}
