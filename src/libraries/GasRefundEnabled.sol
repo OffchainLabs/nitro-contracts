191c40462e60c60d0fc4fba276ac179a49b326a4
gzeon <im@gzeon.dev>
2024-10-03 00:10:49 +0800

    fix: also check in GasRefundEnabled

--- src/libraries/GasRefundEnabled.sol
+++ src/libraries/GasRefundEnabled.sol
@@ -25,7 +25,7 @@ abstract contract GasRefundEnabled {
             // if triggered in a contract call, the spender may be overrefunded by appending dummy data to the call
             // so we check if it is a top level call, which would mean the sender paid calldata as part of tx.input
             // solhint-disable-next-line avoid-tx-origin
-            if (msg.sender != tx.origin) {
+            if (msg.sender != tx.origin || msg.sender.code.length != 0) {
                 // We can't be sure if this calldata came from the top level tx,
                 // so to be safe we tell the gas refunder there was no calldata.
                 calldataSize = 0;
