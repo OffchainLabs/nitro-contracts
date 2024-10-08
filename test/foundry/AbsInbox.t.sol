64813f4aefb2ba0ca6a64b6de7119be68709f36e
gzeon <im@gzeon.dev>
2024-10-09 02:20:19 +0900

    test: codeless origin

--- test/foundry/AbsInbox.t.sol
+++ test/foundry/AbsInbox.t.sol
@@ -215,10 +215,18 @@ abstract contract AbsInboxTest is Test {
         inbox.sendL2MessageFromOrigin(abi.encodePacked("some msg"));
     }
 
-    function test_sendL2MessageFromOrigin_revert_NotCodelessOrigin() public {
-        // TODO: actual test with 7702 in-place
+    function test_sendL2MessageFromOrigin_revert_NotOrigin() public {
         vm.expectRevert(abi.encodeWithSelector(NotCodelessOrigin.selector));
         inbox.sendL2MessageFromOrigin(abi.encodePacked("some msg"));
+    }
+
+    function test_sendL2MessageFromOrigin_revert_NotCodeless() public {
+        assertEq(user.code.length, 0, "user is codeless");
+        vm.etch(user, bytes("some code"));
+        vm.prank(user, user);
+        vm.expectRevert(abi.encodeWithSelector(NotCodelessOrigin.selector));
+        inbox.sendL2MessageFromOrigin(abi.encodePacked("some msg"));
+        vm.etch(user, bytes(""));
     }
 
     function test_sendL2Message() public {
--- test/foundry/SequencerInbox.t.sol
+++ test/foundry/SequencerInbox.t.sol
@@ -437,6 +437,20 @@ contract SequencerInboxTest is Test {
             subMessageCount + 1
         );
 
+        assertEq(rollupOwner.code.length, 0, "rollupOwner is codeless");
+        vm.etch(rollupOwner, bytes("some code"));
+        vm.prank(rollupOwner, rollupOwner);
+        vm.expectRevert(abi.encodeWithSelector(NotCodelessOrigin.selector));
+        seqInbox.addSequencerL2BatchFromOrigin(
+            sequenceNumber,
+            data,
+            delayedMessagesRead,
+            IGasRefunder(address(0)),
+            subMessageCount,
+            subMessageCount + 1
+        );
+        vm.etch(rollupOwner, bytes(""));
+
         vm.prank(rollupOwner);
         seqInbox.setIsBatchPoster(tx.origin, false);
 
