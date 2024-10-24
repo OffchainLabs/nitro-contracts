1078d09d5e834e63e23713cb83c4d39839e20935
gzeon <im@gzeon.dev>
2024-10-24 17:11:19 +0900

    Revert "chore: format"
    This reverts commit 738c3dcab3a89a57b86aa2097cb22a1fb70842fb.

--- test/foundry/ExpressLaneELCRound.t.sol
+++ test/foundry/ExpressLaneELCRound.t.sol
@@ -9,9 +9,7 @@ contract LatestELCRoundsImp {
 
     ELCRound[2] public rounds;
 
-    constructor(
-        ELCRound[2] memory r
-    ) {
+    constructor(ELCRound[2] memory r) {
         rounds[0] = r[0];
         rounds[1] = r[1];
     }
@@ -20,9 +18,7 @@ contract LatestELCRoundsImp {
         return rounds.latestELCRound();
     }
 
-    function resolvedRound(
-        uint64 round
-    ) public view returns (ELCRound memory) {
+    function resolvedRound(uint64 round) public view returns (ELCRound memory) {
         return rounds.resolvedRound(round);
     }
 
@@ -95,10 +91,11 @@ contract ExpressLaneELCRoundTest is Test {
         li.resolvedRound(10);
     }
 
-    function getELCRound(
-        LatestELCRoundsImp li,
-        uint8 index
-    ) internal view returns (ELCRound memory) {
+    function getELCRound(LatestELCRoundsImp li, uint8 index)
+        internal
+        view
+        returns (ELCRound memory)
+    {
         (address elc, uint64 round) = li.rounds(index);
         return ELCRound(elc, round);
     }
