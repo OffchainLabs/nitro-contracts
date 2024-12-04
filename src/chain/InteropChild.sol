3941ea0521e0b7c419012e35267a13d8cc9749c3
gzeon <im@gzeon.dev>
2024-12-04 12:08:04 -0500

    interParent: child templates

---
+++ src/chain/InteropChild.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import "./IInteropChild.sol";
+
+contract InteropChild is IInteropChild {
+    function destfunc(address counter, bytes32 meta) external {
+        // TODO: implement
+    }
+}
