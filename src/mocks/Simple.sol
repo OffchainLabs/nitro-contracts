44949a6e70a6dd977475ca3a5b6edc91cbc401ef
Pepper Lebeck-Jobe <eljobe@gmail.com>
2025-01-27 13:12:20 +0100

    Add the ability to create addresses to the RedeemAll method
    This change also renames the method to ReedeemAllAndCreateAddresses. The reason
    for the complexity of the commit is that we need to have a single top-level
    contract call that will cause many transactions to occur in the same block. Some
    of those transactions should cause accounts to be deleted, and others should
    cause accounts to be created.
    
    This way, we can tickle a bug in the current implementation of validation which
    occurs when account deletions and additions take place out of order and the
    validation logic isn't able to find a preimage for a particular account.

--- src/mocks/Simple.sol
+++ src/mocks/Simple.sol
@@ -45,7 +45,7 @@ contract Simple {
      * @notice Redeems a list of retryable tickets.
      * @param ticketIds The array of retryable ticket identifiers (ticket hashes).
      */
-    function redeemAll(bytes32[] calldata ticketIds) external {
+    function redeemAllAndCreateAddresses(bytes32[] calldata ticketIds, address payable[] calldata addresses) external payable {
         for (uint256 i = 0; i < ticketIds.length; i++) {
             // Attempt to redeem each retryable ticket
             try ArbRetryableTx(address(110)).redeem{gas: 100000}(ticketIds[i]) {
@@ -54,6 +54,9 @@ contract Simple {
             } catch {
                 revert("Failed to redeem one or more tickets");
             }
+        }
+        for (uint256 i = 0; i < addresses.length; i++) {
+            addresses[i].transfer(1);
         }
     }
 
