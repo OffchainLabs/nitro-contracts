9ddd9333d8a5944a21139ffb3938e3c99839a418
Chris Buckland <cpbuckland88@gmail.com>
2025-01-29 17:00:33 +0000

    Cleaned TODOs

--- src/stylus/StylusDeployer.sol
+++ src/stylus/StylusDeployer.sol
@@ -1,24 +1,14 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-// CHRIS: TODO: fix to a specific sol version
+pragma solidity 0.8.28;
 import {ArbWasm} from "../precompiles/ArbWasm.sol";
 
-// 0. scrub useless comments
-// 1. docs and tests - how can we do that? fork test? nope, that wont work either? e2e test with the testnode
-// 2. should this contract be deployable at the same address on all chains? yes, deploy from one of the existing proxies - that's a decent reason to use solidity tbh is it tho, cos we could use an existing deployer i think
-// 3. we want to support create2 so that people can have the same stylus contract on multiple chains if they want to - does that matter? yes for some users
-//    even bother with create1? people can choose a random nonce if they dont care - let them inject a salt, or just use the current account nonce
-//    maybe create2 doesnt make sense for this use case since all have empty constructors? can we use the init code as a salt? yes + a user salt + the value
-
-// CHRIS: TODO: add create2 util getter
-// CHRIS: TODO: get the init return data - either bubble it up in revert if fail or log and return if success
-// CHRIS: TODO: decide whether to add full data to the event: initData, initValue, salt, msg.sender, initReturnVal, whether init occurred
-// CHRIS: TODO: public helper for the salt?
-
+/// @title A Stylus contract deployer, activator and initializer
+/// @author The name of the author
+/// @notice Stylus contracts do not support constructors. Instead, Stylus devs can use this contract to deploy and
+///         initialize their contracts atomically
 contract StylusDeployer {
     ArbWasm constant ARB_WASM = ArbWasm(0x0000000000000000000000000000000000000071);
 
-    // CHRIS: TODO: check all events and errors for usage
     event ContractDeployed(address deployedContract);
 
     error ContractDeploymentError(bytes bytecode);
@@ -35,7 +25,9 @@ contract StylusDeployer {
     ///         Activation is not always necessary. If a contract has the same code has as another recently activated
     ///         contract then activation will be skipped.
     ///         If additional value remains in the contract after activation it will be transferred to the msg.sender
-    ///         to that end the caller must ensure that they can receive eth.
+    ///         to that end the caller must ensure that they can receive eth. Even if they do not expect to receive any value
+    ///         they should still use a payable caller, since value could get forced into the contract using a methods like self destruct
+    ///         or retryable transactions.
     ///
     ///         The caller should do the following before calling this contract:
     ///         1. Check whether the contract will require activation, and if so what the cost will be.
@@ -47,7 +39,6 @@ contract StylusDeployer {
     ///
     ///         Note: Stylus contract caching is not done by the deployer, and will have to be done separately after deployment.
     ///         See https://docs.arbitrum.io/stylus/how-tos/caching-contracts for more details on caching
-    /// @dev CHRIS: TODO
     /// @param bytecode The bytecode of the stylus contract to be deployed
     /// @param initData Initialisation call data. After deployment the contract will be called with this data
     ///                 If no initialisation data is provided then the newly deployed contract will not be called.
@@ -82,10 +73,6 @@ contract StylusDeployer {
 
         // initialize - this will fail if the program wasn't activated by this point
         // we check if initData exists to avoid calling contracts unnecessarily
-        // CHRIS: TODO: test the above comment - and should we give a kinder error for that case? should be a nice one
-        // CHRIS: TODO: test what happens when a stylus contract is called with a empty data - we hit the fallback i guess?
-        //            : we're not gonna support that here, so that needs to be documented
-        // CHRIS: TODO: should we just call everytime?
         if (initData.length != 0) {
             (bool success, ) = address(newContractAddress).call{value: initValue}(initData);
             if (!success) {
@@ -100,12 +87,11 @@ contract StylusDeployer {
         // - activation can return some to this contract
         // - some activation value was supplied but not used
         if (shouldActivate || msg.value != initValue) {
-            // CHRIS: TODO: balance or just the expected amount - someone could force a refund this way otherwise
             uint256 bal = address(this).balance;
             if (bal != 0) {
+                // the caller must be payable, even if they dont expect to receive any balance since it value can be forced into
+                // this contract via selfdestruct
                 (bool sent, ) = payable(msg.sender).call{value: bal}("");
-                // CHRIS: TODO: if we keep this we need to add it to the docs that the caller must be payable
-                // CHRIS: TODO: someone can put funds into the here and cause a revert. Maybe we should calculate instead of using balance
                 if (!sent) {
                     revert RefundExcessValueError(bal);
                 }
@@ -119,7 +105,11 @@ contract StylusDeployer {
         return newContractAddress;
     }
 
-    // CHRIS: TODO: natspec
+    /// @notice When using CREATE2 the deployer includes the init data and value in the salt so that callers
+    ///         can be sure that wherever they encourter this address it was initialized with the same data and value
+    /// @param salt A user supplied salt
+    /// @param initData The init data that will be used to init the deployed contract
+    /// @param initValue The init value that will be used to init the deployed contract
     function initSalt(
         bytes32 salt,
         bytes calldata initData,
@@ -128,6 +118,7 @@ contract StylusDeployer {
         return keccak256(abi.encodePacked(salt, initData, initValue));
     }
 
+    /// @notice Checks whether a contract requires activation
     function requiresActivation(address addr) public view returns (bool) {
         // currently codeHashVersion returns an error when codeHashVersion != stylus version
         // so we do a try/catch to to check it
@@ -138,8 +129,6 @@ contract StylusDeployer {
             // stylus version is always >= 1
             codeHashVersion = 0;
         }
-
-        // CHRIS: TODO: decide whether we make this forward compatible or not - arguably not, then we can get rid of the below check and just return != 0
 
         // due to the bug in ARB_WASM.codeHashVersion we know that codeHashVersion will either be 0 or the current
         // version. We still check that is not equal to the stylusVersion
