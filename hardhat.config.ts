00402933c9279c868fa93620717936459969c009
Henry <11198460+godzillaba@users.noreply.github.com>
2025-06-24 12:30:09 -0600

    remove key from config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -208,7 +208,6 @@ module.exports = {
     },
     geth: {
       url: 'http://localhost:8545',
-      accounts: ['0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80']
     },
   },
   etherscan: {
