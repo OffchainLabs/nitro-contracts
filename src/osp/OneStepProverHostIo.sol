OffchainLabs/nitro-contracts

commit 44b2eb4564c2525cce23b02503ed5041c9c74c12

refactor: avoid assembly

src/osp/OneStepProverHostIo.sol
@@ -239,10 +239,7 @@ contract OneStepProverHostIo is IOneStepProver {
             // Extract certificate size and certificate
             require(customProof.length >= 8, "CUSTOM_DA_PROOF_TOO_SHORT");
 
-            uint256 certSize;
-            assembly {
-                certSize := shr(192, calldataload(add(customProof.offset, 0))) // Read 8 bytes
-            }
+            uint256 certSize = uint256(uint64(bytes8(customProof[0:8])));
 
             require(customProof.length >= 8 + certSize, "PROOF_TOO_SHORT_FOR_CERT");
 
@@ -314,10 +311,7 @@ contract OneStepProverHostIo is IOneStepProver {
         uint256 proofOffset,
         bytes32 expectedHash
     ) internal view returns (bool) {
-        uint256 certSize;
-        assembly {
-            certSize := shr(192, calldataload(add(proof.offset, add(proofOffset, 0))))
-        }
+        uint256 certSize = uint256(uint64(bytes8(proof[0:8])));
 
         require(proof.length >= proofOffset + 8 + certSize + 1, "PROOF_TOO_SHORT");
 

commit 1160b955cae8e23bb650a4ba274ec9be38518df7

Fix: Read certificate size from correct offset in validateAndCheckCertificate
Bug was introduced in 44b2eb4 when refactoring from assembly.
The certSize was being read from proof[0:] instead of proof[proofOffset:],
causing PROOF_TOO_SHORT errors when the validator tried to use garbage
data as the certificate size.

src/osp/OneStepProverHostIo.sol
@@ -316,7 +316,7 @@ contract OneStepProverHostIo is IOneStepProver {
         uint256 proofOffset,
         bytes32 expectedHash
     ) internal view returns (bool) {
-        uint256 certSize = uint256(uint64(bytes8(proof[0:CERT_SIZE_LEN])));
+        uint256 certSize = uint256(uint64(bytes8(proof[proofOffset:proofOffset + CERT_SIZE_LEN])));
 
         require(
             proof.length >= proofOffset + CERT_SIZE_LEN + certSize + CLAIMED_VALID_LEN,