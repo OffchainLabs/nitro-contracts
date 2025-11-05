8fa264f4cd4403acdc9e000c75492ad22c99b2ff
gzeon <im@gzeon.dev>
2025-11-05 19:49:09 +0800

    fix: use proper interface

--- src/chain/ResourceConstraintManager.sol
+++ src/chain/ResourceConstraintManager.sol
@@ -14,33 +14,40 @@ contract ResourceConstraintManager is AccessControlEnumerable {
 
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
 
+    error TooManyConstraints();
+    error InvalidPeriod(uint64[3] constraint);
+    error InvalidTarget(uint64[3] constraint);
+
     constructor(address admin, address executor) {
         _setupRole(DEFAULT_ADMIN_ROLE, admin);
         _setupRole(MANAGER_ROLE, admin);
         _setupRole(MANAGER_ROLE, executor);
     }
 
-    /// @notice Adds or updates a resource constraint
-    /// @param resources an array of resource–weight pairs (see Nitro documentation for the list of resources)
-    /// @param periodSecs the time window for the constraint
-    /// @param targetPerSec allowed usage per second across weighted resources
-    function setResourceConstraint(
-        ArbResourceConstraintsTypes.ResourceWeight[] calldata resources,
-        uint32 periodSecs,
-        uint64 targetPerSec
+    /// @notice Sets the list of gas pricing constraints for the multi-constraint pricing model.
+    ///         See ArbOwner.setGasPricingConstraints interface for more information.
+    /// @param constraints Array of triples (gas_target_per_second, adjustment_window_seconds, starting_backlog_value)
+    ///        - gas_target_per_second: target gas usage per second for the constraint (uint64, gas/sec)
+    ///        - adjustment_window_seconds: time over which the price will rise by a factor of e if demand is 2x the target (uint64, seconds)
+    ///        - starting_backlog_value: initial backlog for this constraint (uint64, gas units)
+    function setGasPricingConstraints(
+        uint64[3][] calldata constraints
     ) external onlyRole(MANAGER_ROLE) {
-        // TODO: restrict input
-        ARB_OWNER.setResourceConstraint(resources, periodSecs, targetPerSec);
-    }
-
-    /// @notice Removes a resource constraint
-    /// @param resources the list of resource kinds to be removed (see Nitro documentation for the list of resources)
-    /// @param periodSecs the time window for the constraint
-    function clearConstraint(
-        ArbResourceConstraintsTypes.ResourceKind[] calldata resources,
-        uint32 periodSecs
-    ) external onlyRole(MANAGER_ROLE) {
-        // TODO: restrict input
-        ARB_OWNER.clearConstraint(resources, periodSecs);
+        // If zero constraints are provided, the chain uses the single-constraint pricing model
+        if (constraints.length > 10) {
+            revert TooManyConstraints();
+        }
+        uint256 nConstraints = constraints.length;
+        for (uint256 i = 0; i < nConstraints; ++i) {
+            uint64 gasTargetPerSec = constraints[i][0];
+            uint64 adjustmentWindowSecs = constraints[i][1];
+            if (gasTargetPerSec < 7_000_000 || gasTargetPerSec > 100_000_000) {
+                revert InvalidTarget(constraints[i]);
+            }
+            if (adjustmentWindowSecs < 5 || adjustmentWindowSecs > 86400) {
+                revert InvalidPeriod(constraints[i]);
+            }
+        }
+        ARB_OWNER.setGasPricingConstraints(constraints);
     }
 }
