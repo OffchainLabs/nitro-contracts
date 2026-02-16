8245a200c4dc059cde14b2a6b5d88c37dd85bde7
Chris Buckland <cpbuckland88@gmail.com>
2026-02-16 13:11:26 +0000

    refactor: simplify test script with set -e, trap, and tee

--- scripts/testUpgrade.bash
+++ scripts/testUpgrade.bash
@@ -1,35 +1,18 @@
 #!/bin/bash
+set -eo pipefail
 
 ANVILFORK=true
 export ANVILFORK
 
 anvil --fork-url $L1_RPC > /dev/null &
-
 anvil_pid=$!
+trap 'kill $anvil_pid' EXIT
 
-yarn script:bold-prepare && \
-yarn script:bold-populate-lookup && \
-execute_output=$(yarn script:bold-local-execute 2>&1)
-ecode=$?
-echo "$execute_output"
+yarn script:bold-prepare
+yarn script:bold-populate-lookup
+yarn script:bold-local-execute 2>&1 | tee execute.log
 
-if [ $ecode -ne 0 ]; then
-  kill $anvil_pid
-  exit $ecode
-fi
+tx_hash=$(grep 'upgrade tx hash:' execute.log | awk '{print $NF}')
+rm -f execute.log
 
-# Test standalone verification by re-running with the tx hash from execute
-tx_hash=$(echo "$execute_output" | grep 'upgrade tx hash:' | awk '{print $NF}')
-if [ -z "$tx_hash" ]; then
-  echo "FAIL: could not extract tx hash from execute output"
-  kill $anvil_pid
-  exit 1
-fi
-
-echo "testing standalone verification with TX_HASH=$tx_hash"
 TX_HASH=$tx_hash yarn script:bold-verify
-ecode=$?
-
-kill $anvil_pid
-
-exit $ecode
