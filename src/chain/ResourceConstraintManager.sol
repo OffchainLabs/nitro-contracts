40f7aed958edc705dbf7b7678abb0b178b313bfd
TucksonDev <tucksondev@gmail.com>
2026-03-06 12:33:44 +0000

    Add function to manage setMultiGasPricingConstraints from the ResourceConstraintManager

--- src/chain/ResourceConstraintManager.sol
+++ src/chain/ResourceConstraintManager.sol
@@ -12,6 +12,14 @@ contract ResourceConstraintManager is AccessControlEnumerable {
     ArbOwner internal constant ARB_OWNER = ArbOwner(address(0x70));
     ArbGasInfo internal constant ARB_GAS_INFO = ArbGasInfo(address(0x6c));
 
+    // Constraint parameters boundaries
+    uint256 public constant MAX_CONSTRAINTS = 10;
+    uint64 public constant MIN_GAS_TARGET_PER_SEC = 7_000_000;
+    uint64 public constant MAX_GAS_TARGET_PER_SEC = 100_000_000;
+    uint32 public constant MIN_ADJUSTMENT_WINDOW_SECS = 5;
+    uint32 public constant MAX_ADJUSTMENT_WINDOW_SECS = 86400;
+    uint64 public constant MAX_PRICING_EXPONENT = 8000; // scaled by 1000 to allow for fractional exponents
+
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE");
     uint256 public expiryTimestamp;
 
@@ -84,5 +92,75 @@ contract ResourceConstraintManager is AccessControlEnumerable {
         }
 
         ARB_OWNER.setGasPricingConstraints(constraints);
+    }
+
+    /// @notice Sets the list of multi-gas pricing constraints for the multi-dimensional multi-constraint pricing model.
+    ///         See ArbOwner.setMultiGasPricingConstraints interface for more information.
+    /// @param constraints Array of ResourceConstraint structs, each containing:
+    ///        - resources: list of (ResourceKind, weight) pairs
+    ///        - adjustmentWindowSecs: time window (seconds) over which the price will rise by a factor of e if demand is 2x the target (uint32, seconds)
+    ///        - targetPerSec: target gas usage per second for this constraint (uint64, gas/sec)
+    ///        - backlog: initial backlog value for this constraint (uint64, gas units)
+    function setMultiGasPricingConstraints(
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] calldata constraints
+    ) external onlyRole(MANAGER_ROLE) {
+        // If zero constraints are provided, the chain uses the single-constraint pricing model
+        // Starting from ArbOS 60, there's no limit to the number of constraints to set
+        uint256 nConstraints = constraints.length;
+
+        // We calculate the implied pricing exponent for each resource kind
+        uint8 numResourceKinds = uint8(type(ArbMultiGasConstraintsTypes.ResourceKind).max) + 1;
+        uint64[] memory pricingExponents = new uint64[](numResourceKinds);
+        for (uint256 i = 0; i < nConstraints; ++i) {
+            uint64 targetPerSec = constraints[i].targetPerSec;
+            uint32 adjustmentWindowSecs = constraints[i].adjustmentWindowSecs;
+            uint64 startingBacklogValue = constraints[i].backlog;
+            if (targetPerSec < MIN_GAS_TARGET_PER_SEC || targetPerSec > MAX_GAS_TARGET_PER_SEC) {
+                revert InvalidTarget(targetPerSec, adjustmentWindowSecs, startingBacklogValue);
+            }
+            if (adjustmentWindowSecs < MIN_ADJUSTMENT_WINDOW_SECS || adjustmentWindowSecs > MAX_ADJUSTMENT_WINDOW_SECS) {
+                revert InvalidPeriod(targetPerSec, adjustmentWindowSecs, startingBacklogValue);
+            }
+            if (startingBacklogValue > 0) {
+                // Find the maximum weight among all resources in this constraint
+                uint64 maxWeight = 0;
+                uint256 nResources = constraints[i].resources.length;
+                for (uint256 j = 0; j < nResources; ++j) {
+                    uint64 weight = constraints[i].resources[j].weight;
+                    if (weight > maxWeight) {
+                        maxWeight = weight;
+                    }
+                }
+
+                if (maxWeight > 0) {
+                    // Neither of these values can be zero due to the earlier checks, so this division is safe
+                    uint256 divisor = uint256(adjustmentWindowSecs) * uint256(targetPerSec) * uint256(maxWeight);
+
+                    // Calculate per-resource-kind exponent contribution
+                    // we scale by 1000 to improve precision in calculating the exponent
+                    // since this division will round down, it's always possible for the real exponent to be up to
+                    // the number of constraints greater than the value we measure
+                    // Operation is performed in uint256 to avoid overflow, but the result is guaranteed to fit in uint64 due to the earlier check
+                    for (uint256 j = 0; j < nResources; ++j) {
+                        uint8 kind = uint8(constraints[i].resources[j].resource);
+                        uint64 weight = constraints[i].resources[j].weight;
+                        pricingExponents[kind] += uint64(uint256(startingBacklogValue) * uint256(weight) * 1000 / divisor);
+                    }
+                }
+            }
+        }
+
+        // this calculated pricing exponent will by used by nitro to calculate the gas price
+        // we check that the pricing exponent is below some reasonable number to avoid setting the gas price astronomically high
+        // as long as the gas price is not so high that no-one at all can send a transaction the chain will be able to function
+        // eg. these constraints can be changed again, or the sec council can send admin transactions
+        // with min base fee of 0.02, exponent of 8 (scaled by 1000) corresponds to a gas price of ~60 Gwei
+        for (uint8 k = 0; k < numResourceKinds; ++k) {
+            if (pricingExponents[k] > MAX_PRICING_EXPONENT) {
+                revert PricingExponentTooHigh(pricingExponents[k]);
+            }
+        }
+
+        ARB_OWNER.setMultiGasPricingConstraints(constraints);
     }
 }
--- test/foundry/ResourceConstraintManager.t.sol
+++ test/foundry/ResourceConstraintManager.t.sol
@@ -30,6 +30,9 @@ contract ResourceConstraintManagerTest is Test {
         assertTrue(ARB_OWNER.removeChainOwnerCalled());
     }
 
+    //
+    // --- setGasPricingConstraints tests ---
+    //
     function test_setGasPricingConstraints_success() external {
         // Test with valid single constraint
         uint64[3][] memory constraints = new uint64[3][](1);
@@ -234,11 +237,290 @@ contract ResourceConstraintManagerTest is Test {
         );
         resourceConstraintManager.setGasPricingConstraints(constraints);
     }
+
+    //
+    // --- setMultiGasPricingConstraints tests ---
+    //
+    function _createMultiGasConstraintWithResources(
+        uint64 targetPerSec,
+        uint32 adjustmentWindowSecs,
+        uint64 backlog,
+        ArbMultiGasConstraintsTypes.WeightedResource[] memory resources
+    ) internal pure returns (ArbMultiGasConstraintsTypes.ResourceConstraint memory) {
+        return ArbMultiGasConstraintsTypes.ResourceConstraint({
+            resources: resources,
+            adjustmentWindowSecs: adjustmentWindowSecs,
+            targetPerSec: targetPerSec,
+            backlog: backlog
+        });
+    }
+
+    function _createMultiGasConstraint(
+        uint64 targetPerSec,
+        uint32 adjustmentWindowSecs,
+        uint64 backlog
+    ) internal pure returns (ArbMultiGasConstraintsTypes.ResourceConstraint memory) {
+        ArbMultiGasConstraintsTypes.WeightedResource[] memory resources =
+            new ArbMultiGasConstraintsTypes.WeightedResource[](1);
+        resources[0] = ArbMultiGasConstraintsTypes.WeightedResource({
+            resource: ArbMultiGasConstraintsTypes.ResourceKind.Computation,
+            weight: 1
+        });
+        return _createMultiGasConstraintWithResources(targetPerSec, adjustmentWindowSecs, backlog, resources);
+    }
+
+    function test_setMultiGasPricingConstraints_success() external {
+        // Test with valid single constraint using single resource kind
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraints[0] = _createMultiGasConstraint(10_000_000, 100, 0);
+
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+
+        // Test with multiple constraints using different resource kinds and weights
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory multipleConstraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](3);
+
+        // Single resource: Computation with weight 1
+        multipleConstraints[0] = _createMultiGasConstraint(7_000_000, 5, 0);
+
+        // Multiple resources: Computation (weight 2) + StorageAccess (weight 3)
+        ArbMultiGasConstraintsTypes.WeightedResource[] memory resources2 =
+            new ArbMultiGasConstraintsTypes.WeightedResource[](2);
+        resources2[0] = ArbMultiGasConstraintsTypes.WeightedResource({
+            resource: ArbMultiGasConstraintsTypes.ResourceKind.HistoryGrowth,
+            weight: 1
+        });
+        resources2[1] = ArbMultiGasConstraintsTypes.WeightedResource({
+            resource: ArbMultiGasConstraintsTypes.ResourceKind.StorageAccess,
+            weight: 1
+        });
+        multipleConstraints[1] = _createMultiGasConstraintWithResources(50_000_000, 1000, 1, resources2);
+
+        // Multiple resources: HistoryGrowth (weight 5) + L1Calldata (weight 1)
+        ArbMultiGasConstraintsTypes.WeightedResource[] memory resources3 =
+            new ArbMultiGasConstraintsTypes.WeightedResource[](2);
+        resources3[0] = ArbMultiGasConstraintsTypes.WeightedResource({
+            resource: ArbMultiGasConstraintsTypes.ResourceKind.WasmComputation,
+            weight: 5
+        });
+        resources3[1] = ArbMultiGasConstraintsTypes.WeightedResource({
+            resource: ArbMultiGasConstraintsTypes.ResourceKind.L1Calldata,
+            weight: 1
+        });
+        multipleConstraints[2] = _createMultiGasConstraintWithResources(100_000_000, 86400, 10000, resources3);
+
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(multipleConstraints);
+
+        // Test with empty constraints array (switch to previous pricing model)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory emptyConstraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](0);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(emptyConstraints);
+    }
+
+    function test_setMultiGasPricingConstraints_pricingExponentTooHigh() external {
+        // create constraints on the limit of the pricing exponent
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory multipleConstraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](3);
+        multipleConstraints[0] = _createMultiGasConstraint(7_000_000, 5, 35_000_000); // 1000
+        multipleConstraints[1] = _createMultiGasConstraint(50_000_000, 1000, 300_000_000_000); // 6000
+        multipleConstraints[2] = _createMultiGasConstraint(100_000_000, 86400, 8_640_000_000_000); // 1000
+
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(multipleConstraints);
+
+        // up to the limit
+        multipleConstraints[1] = _createMultiGasConstraint(50_000_000, 1000, 300_049_999_999);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(multipleConstraints);
+
+        // over the limit
+        multipleConstraints[1] = _createMultiGasConstraint(50_000_000, 1000, 300_050_000_000);
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(ResourceConstraintManager.PricingExponentTooHigh.selector, 8001)
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(multipleConstraints);
+    }
+
+    function test_setMultiGasPricingConstraints_accessControl() external {
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraints[0] = _createMultiGasConstraint(10_000_000, 100, 0);
+
+        // Test non-manager cannot call
+        vm.expectRevert();
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+
+        // Test admin without manager role cannot call
+        vm.prank(admin);
+        vm.expectRevert();
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+
+        // Test manager can call
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+    }
+
+    function test_setMultiGasPricingConstraints_noConstraintLimit() external {
+        // Starting from ArbOS 60, there's no limit to the number of constraints
+        // Test 10 constraints (should succeed)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory tenConstraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](10);
+        for (uint256 i = 0; i < 10; i++) {
+            tenConstraints[i] = _createMultiGasConstraint(10_000_000, 100, 0);
+        }
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(tenConstraints);
+
+        // Test 11 constraints (should also succeed)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory elevenConstraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](11);
+        for (uint256 i = 0; i < 11; i++) {
+            elevenConstraints[i] = _createMultiGasConstraint(10_000_000, 100, 0);
+        }
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(elevenConstraints);
+    }
+
+    function test_setMultiGasPricingConstraints_invalidTarget() external {
+        // Test gas target below minimum (6,999,999)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsLowTarget =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsLowTarget[0] = _createMultiGasConstraint(6_999_999, 100, 0);
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidTarget.selector,
+                uint64(6_999_999),
+                uint64(100),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsLowTarget);
+
+        // Test gas target above maximum (100,000,001)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsHighTarget =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsHighTarget[0] = _createMultiGasConstraint(100_000_001, 100, 0);
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidTarget.selector,
+                uint64(100_000_001),
+                uint64(100),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsHighTarget);
+
+        // Test edge cases (exactly at boundaries should succeed)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsMinTarget =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsMinTarget[0] = _createMultiGasConstraint(7_000_000, 100, 0);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsMinTarget);
+
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsMaxTarget =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsMaxTarget[0] = _createMultiGasConstraint(100_000_000, 100, 0);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsMaxTarget);
+    }
+
+    function test_setMultiGasPricingConstraints_invalidPeriod() external {
+        // Test adjustment window below minimum (4 seconds)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsLowPeriod =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsLowPeriod[0] = _createMultiGasConstraint(10_000_000, 4, 0);
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidPeriod.selector,
+                uint64(10_000_000),
+                uint64(4),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsLowPeriod);
+
+        // Test adjustment window above maximum (86401 seconds)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsHighPeriod =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsHighPeriod[0] = _createMultiGasConstraint(10_000_000, 86401, 0);
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidPeriod.selector,
+                uint64(10_000_000),
+                uint64(86401),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsHighPeriod);
+
+        // Test edge cases (exactly at boundaries should succeed)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsMinPeriod =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsMinPeriod[0] = _createMultiGasConstraint(10_000_000, 5, 0);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsMinPeriod);
+
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraintsMaxPeriod =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](1);
+        constraintsMaxPeriod[0] = _createMultiGasConstraint(10_000_000, 86400, 0);
+        vm.prank(manager);
+        resourceConstraintManager.setMultiGasPricingConstraints(constraintsMaxPeriod);
+    }
+
+    function test_setMultiGasPricingConstraints_multipleConstraintValidation() external {
+        // Test that all constraints are validated (not just the first one)
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] memory constraints =
+            new ArbMultiGasConstraintsTypes.ResourceConstraint[](3);
+        constraints[0] = _createMultiGasConstraint(10_000_000, 100, 0); // Valid
+        constraints[1] = _createMultiGasConstraint(20_000_000, 200, 0); // Valid
+        constraints[2] = _createMultiGasConstraint(5_000_000, 100, 0); // Invalid target
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidTarget.selector,
+                uint64(5_000_000),
+                uint64(100),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+
+        // Test with invalid period in middle
+        constraints[0] = _createMultiGasConstraint(10_000_000, 100, 0); // Valid
+        constraints[1] = _createMultiGasConstraint(20_000_000, 3, 0); // Invalid period
+        constraints[2] = _createMultiGasConstraint(30_000_000, 100, 0); // Valid
+
+        vm.prank(manager);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ResourceConstraintManager.InvalidPeriod.selector,
+                uint64(20_000_000),
+                uint64(3),
+                uint64(0)
+            )
+        );
+        resourceConstraintManager.setMultiGasPricingConstraints(constraints);
+    }
 }
 
 contract ArbOwnerMock {
     bool public removeChainOwnerCalled;
     uint64[3][] public lastConstraints;
+    bytes internal lastMultiGasConstraintsEncoded;
 
     function removeChainOwner(
         address ownerToRemove
@@ -254,5 +536,25 @@ contract ArbOwnerMock {
 
     function getLastConstraints() external view returns (uint64[3][] memory) {
         return lastConstraints;
+    }
+
+    function setMultiGasPricingConstraints(
+        ArbMultiGasConstraintsTypes.ResourceConstraint[] calldata constraints
+    ) external {
+        lastMultiGasConstraintsEncoded = abi.encode(constraints);
+    }
+
+    function getLastMultiGasConstraints()
+        external
+        view
+        returns (ArbMultiGasConstraintsTypes.ResourceConstraint[] memory)
+    {
+        if (lastMultiGasConstraintsEncoded.length == 0) {
+            return new ArbMultiGasConstraintsTypes.ResourceConstraint[](0);
+        }
+        return abi.decode(
+            lastMultiGasConstraintsEncoded,
+            (ArbMultiGasConstraintsTypes.ResourceConstraint[])
+        );
     }
 }
